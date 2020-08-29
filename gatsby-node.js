7fd264e0ec6e17e58be57906d845d99ca3bb7ff6
vUnAmp <60809799+vUnAmp@users.noreply.github.com>
2020-08-29 23:36:41 +0200

    shopify modified

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,39 +2,39 @@ const path = require(`path`)
 
 // Build live site
 
-// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-//   if (stage === 'build-html') {
-//     actions.setWebpackConfig({
-//       module: {
-//         rules: [
-//           {
-//             test: /firebase/,
-//             use: loaders.null(),
-//           },
-//         ],
-//       },
-//     })
-//   }
-// }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /firebase/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
 
 //Develop Only
 
-exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  actions.setWebpackConfig({
-    externals: getConfig().externals.concat(function(
-      context,
-      request,
-      callback
-    ) {
-      const regex = /^firebase(\/(.+))?/
-      // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
-      if (regex.test(request)) {
-        return callback(null, 'umd ' + request)
-      }
-      callback()
-    }),
-  })
-}
+// exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+//   actions.setWebpackConfig({
+//     externals: getConfig().externals.concat(function(
+//       context,
+//       request,
+//       callback
+//     ) {
+//       const regex = /^firebase(\/(.+))?/
+//       // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+//       if (regex.test(request)) {
+//         return callback(null, 'umd ' + request)
+//       }
+//       callback()
+//     }),
+//   })
+// }
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
